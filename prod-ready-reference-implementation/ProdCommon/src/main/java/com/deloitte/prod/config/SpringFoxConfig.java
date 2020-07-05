a51022c919c7410e863d298d7fe7ddb588910605
deepsahoo <deepsahoo@USBLRDEEPSAH009.us.deloitte.com>
2020-07-05 14:26:22 +0530

    develop : confilct resolved

--- prod-ready-reference-implementation/ProdCommon/src/main/java/com/deloitte/prod/config/SpringFoxConfig.java
+++ prod-ready-reference-implementation/ProdCommon/src/main/java/com/deloitte/prod/config/SpringFoxConfig.java
@@ -15,13 +15,6 @@ public class SpringFoxConfig {
 
 	@Bean
     public Docket api() { 
-		/*
-		 * return new Docket(DocumentationType.SWAGGER_2)
-		 * 
-		 * .select().apis(RequestHandlerSelectors.basePackage(
-		 * "com.deloitte.prod.controller")) .paths(regex("/product.*")) .build();
-		 */
-        
         return new Docket(DocumentationType.SWAGGER_2)  
                 .select()                                  
                 .apis(RequestHandlerSelectors.any())              
